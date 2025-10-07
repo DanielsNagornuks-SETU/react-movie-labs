03e2c0bb0bdec89e0db1c0f5a108d7c86dab6800
Daniels Nagornuks <20106595@mail.wit.ie>
2025-10-07 12:12:39 +0100

    Full review page.

---
+++ movies/src/pages/movieReviewPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useLocation } from "react-router";
+import PageTemplate from "../components/templateMoviePage";
+import MovieReview from "../components/movieReview";
+
+function MovieReviewPage(props) {
+    let location = useLocation();
+    const { movie, review } = location.state;
+    return (
+        <PageTemplate movie={movie}>
+            <MovieReview review={review} />
+        </PageTemplate>
+    );
+};
+
+export default MovieReviewPage;
