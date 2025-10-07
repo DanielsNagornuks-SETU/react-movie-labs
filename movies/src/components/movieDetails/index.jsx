ff6c4f8f1fa31aca1ce1457e239219e65a9700e5
Daniels Nagornuks <20106595@mail.wit.ie>
2025-10-07 12:29:29 +0100

    Exercise: Movie Details Page - included production countries.

--- movies/src/components/movieDetails/index.jsx
+++ movies/src/components/movieDetails/index.jsx
@@ -58,6 +58,19 @@ function MovieDetails({ movie }) {
                 />
                 <Chip label={`Released: ${movie.release_date}`} />
             </Paper>
+            <Paper
+                component="ul"
+                sx={{ ...root }}
+            >
+                <li>
+                    <Chip label="Production Countries" sx={{ ...chip }} color="primary" />
+                </li>
+                {movie.production_countries.map((pc) => (
+                    <li key={pc.name}>
+                        <Chip label={pc.name} sx={{ ...chip }} />
+                    </li>
+                ))}
+            </Paper>
             <Fab
                 color="secondary"
                 variant="extended"
