8366ba9e67813d353d1ef8e755001aa68ba48c7a
Daniels Nagornuks <20106595@mail.wit.ie>
2025-10-28 12:24:33 +0000

    Change Site Header Appearance

--- movies/src/components/siteHeader/index.jsx
+++ movies/src/components/siteHeader/index.jsx
@@ -11,6 +11,7 @@ import { useNavigate } from "react-router";
 import { styled } from '@mui/material/styles';
 import { useTheme } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import MovieIcon from '@mui/icons-material/Movie';
 
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
@@ -23,13 +24,17 @@ function SiteHeader() {
 
     const navigate = useNavigate();
 
-    const menuOptions = [
+    const mainMenuOptions = [
         { label: "Home", path: "/" },
-        { label: "Favorites", path: "/movies/favorites" },
         { label: "Upcoming", path: "/movies/upcoming" },
         { label: "Trending", path: "/movies/trending" },
         { label: "Now Playing", path: "/movies/now_playing" },
         { label: "Top Rated", path: "/movies/top_rated" },
+    ];
+
+    const additionalMenuOptions = [
+        { label: "Favorites", path: "/movies/favorites" },
+        { label: "Playlist", path: "/movies/playlist" }
     ];
 
     function handleMenuSelect(pageURL) {
@@ -43,63 +48,73 @@ function SiteHeader() {
 
     return (
         <>
-            <AppBar position="fixed" color="secondary">
-                <Toolbar>
-                    <Typography variant="h4" sx={{ flexGrow: 1 }}>
+            <AppBar position="fixed">
+                <Toolbar sx={{ justifyContent: 'space-between' }}>
+                    <Typography variant="h5" sx={{ fontWeight: 'bold', padding: 0.5 }}>
                         TMDB Client
                     </Typography>
-                    <Typography variant="h6" sx={{ flexGrow: 1 }}>
-                        All you ever wanted to know about Movies!
-                    </Typography>
-                    {isMobile ? (
-                        <>
-                            <IconButton
-                                aria-label="menu"
-                                aria-controls="menu-appbar"
-                                aria-haspopup="true"
-                                onClick={handleMenu}
-                                color="inherit"
-                            >
-                                <MenuIcon />
-                            </IconButton>
-                            <Menu
-                                id="menu-appbar"
-                                anchorEl={anchorEl}
-                                anchorOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: "top",
-                                    horizontal: "right",
-                                }}
-                                open={open}
-                                onClose={() => setAnchorEl(null)}
-                            >
-                                {menuOptions.map((opt) => (
-                                    <MenuItem
+                    <Typography sx={{ flexGrow: 1 }}>
+                        {isMobile ? (
+                            <>
+                                <IconButton
+                                    aria-label="menu"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleMenu}
+                                    color="inherit"
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorEl}
+                                    anchorOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: "top",
+                                        horizontal: "right",
+                                    }}
+                                    open={open}
+                                    onClose={() => setAnchorEl(null)}
+                                >
+                                    {mainMenuOptions.map((opt) => (
+                                        <MenuItem
+                                            key={opt.label}
+                                            onClick={() => handleMenuSelect(opt.path)}
+                                        >
+                                            {opt.label}
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </>
+                        ) : (
+                            <>
+                                {mainMenuOptions.map((opt) => (
+                                    <Button
                                         key={opt.label}
+                                        color="inherit"
                                         onClick={() => handleMenuSelect(opt.path)}
                                     >
                                         {opt.label}
-                                    </MenuItem>
+                                    </Button>
                                 ))}
-                            </Menu>
-                        </>
-                    ) : (
-                        <>
-                            {menuOptions.map((opt) => (
-                                <Button
-                                    key={opt.label}
-                                    color="inherit"
-                                    onClick={() => handleMenuSelect(opt.path)}
-                                >
-                                    {opt.label}
-                                </Button>
-                            ))}
-                        </>
-                    )}
+                            </>
+                        )}
+                    </Typography>
+                    <Typography>
+                        {additionalMenuOptions.map((opt) => (
+                            <Button
+                                key={opt.label}
+                                color="inherit"
+                                onClick={() => handleMenuSelect(opt.path)}
+                            >
+                                {opt.label}
+                            </Button>
+                        ))}
+                    </Typography>
                 </Toolbar>
             </AppBar>
             <Offset />
